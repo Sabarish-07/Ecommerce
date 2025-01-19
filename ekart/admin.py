07db999a0817037c0a5b604c353f745b61d6675f
SABARISH A <sabarishamirtharaj07@gmail.com>
2025-01-19 19:10:27 +0530

    Add files via upload

---
+++ ekart/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from.models import *
+admin.site.register(Product)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
+admin.site.register(Contact)
+# Register your models here.
